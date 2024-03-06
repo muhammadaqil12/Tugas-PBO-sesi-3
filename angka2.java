4226b9496ee0f58c86ec180c906ca0c179714209
muhammadaqil12 <115065268+muhammadaqil12@users.noreply.github.com>
2024-03-06 20:38:11 +0700

    Create angka2.java

---
+++ angka2.java
@@ -0,0 +1,13 @@
+public class angka2 {
+    
+    public static void main(String[] argume){
+      
+        // create an array
+        int[] numbers = {9, 6 ,3 ,0 -3 -6};
+        
+        // iterating through the array 
+        for (int number: numbers) {
+           System.out.println(number);
+        }
+      }
+}
