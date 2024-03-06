2c3d2d56966b07cf245fe16f4c2035b6063d58f4
muhammadaqil12 <115065268+muhammadaqil12@users.noreply.github.com>
2024-03-06 20:39:44 +0700

    Create perkalian.java

---
+++ perkalian.java
@@ -0,0 +1,31 @@
+public class perkalian {
+
+
+    
+    public static void main(String[] argume){
+        
+                int num = 4; 
+                int limit = 4; // Batas perkalian
+                
+                
+                for (int i = 1; i <= limit; i++) {
+                    int perkalian = num * i;
+                    System.out.println(num + " * " + i + " = " + perkalian);
+                }
+    
+                int base = 99; // Angka yang akan dikalikan
+                
+
+                for (int i = 20; i <= 24; i++) {
+                    int kali = base * i;
+
+                    System.out.println (base + " * " + i + " = " + kali);
+
+                }
+    }
+}    
+            
+
+
+        
+        
