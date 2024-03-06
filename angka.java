359ad07281a535509fb08572cd36ddf4868d29ad
muhammadaqil12 <115065268+muhammadaqil12@users.noreply.github.com>
2024-03-06 20:34:43 +0700

    Create angka.java

---
+++ angka.java
@@ -0,0 +1,10 @@
+public class angka {
+    
+    public static void main(String[] argume){
+
+        for(int i=0; i <= 11; i++){
+            System.out.print( i + " " );
+        }
+
+    }
+    }
